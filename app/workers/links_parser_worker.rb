d1c67ad1a618b437a98e5934963c73620070cb5f
rghost <miarhost@gmail.com>
2024-01-18 20:06:43 +0200

    Add details to parser worker and its logger, saving links to mongo record

--- app/workers/links_parser_worker.rb
+++ app/workers/links_parser_worker.rb
@@ -5,23 +5,28 @@ class LinksParserWorker
   include MongoClient
   include SneakersLogging
 
-  from_queue :parsing
+  from_queue :parsing, threads: 50, prefetch: 50, timeout_job_after: 1
 
-  def work_with_params(data, delivery_info, metadata)
+  def work_with_params(message, delivery_info, metadata)
+    data = JSON.parse(message)
     worker_trace "received #{data} at #{Time.now}"
-    save_to_list_record(scrap(data), data[:url], data[:topic])
+    links_list = save_to_list_record(scrap(data), data["url"], data["topic"])
     logging(data, delivery_info, metadata)
+
   rescue => e
     error_messaging(e, delivery_info)
     ack!
   end
 
   def scrap(data)
-    resource = Faraday.get(data[:url])
-    puts resource.body
-    doc = Nokogiri::HTML(open(resource.body))
-    doc.css('a href')
-      .select { |i| i['class'] == "u-tcGrayDarkest js-work-link" }
+    resource = Faraday.get(data["url"])
+    doc = Nokogiri::HTML(resource.body)
+    links = []
+    attrs = doc.css('div.download')
+    attrs.each do |item|
+      links << item.css("a").attribute("href").value
+    end
+    links
   end
 
   def save_to_list_record(links, url, topic)
--- app/workers/links_publisher_worker.rb
+++ app/workers/links_publisher_worker.rb
@@ -4,11 +4,11 @@ class LinksPublisherWorker
   include Sneakers::Worker
   include SneakersLogging
 
-  from_queue :parsing
+  from_queue :parsing, threads: 25, prefetch: 25, timeout_job_after: 1
 
-  def work_with_params(data=ParsedList.last, delivery_info, metadata)
-    publish(data, to_queue: 'parsing')
-    worker_trace "sending links by request #{data}"
+  def work_with_params(data=ParsedList.last.document, delivery_info, metadata)
+    publish(data.to_json, to_queue: 'parsed')
+    worker_trace "sending links by request: #{data}"
     logging(data, delivery_info, metadata)
   rescue => e
     error_messaging(e, delivery_info)
