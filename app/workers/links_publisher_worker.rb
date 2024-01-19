9d4ca27e5dbc23deaf4f47d29f9c9dd70aa902b2
rghost <miarhost@gmail.com>
2024-01-19 18:55:11 +0200

    Redirect parsed publisher messaging and add test for

--- app/workers/links_publisher_worker.rb
+++ app/workers/links_publisher_worker.rb
@@ -4,14 +4,15 @@ class LinksPublisherWorker
   include Sneakers::Worker
   include SneakersLogging
 
-  from_queue :parsing, threads: 25, prefetch: 25, timeout_job_after: 1
+  from_queue :parsed, threads: 25, prefetch: 25, timeout_job_after: 1
 
-  def work_with_params(data=ParsedList.last.document, delivery_info, metadata)
-    publish(data.to_json, to_queue: 'parsed')
+  def work_with_params(_message, delivery_info, metadata)
+    data = ParsedList.last.document.take(5)
+    publish(JSON.generate(data), to_queue: 'parsed', routing_key: 'parsed')
     worker_trace "sending links by request: #{data}"
     logging(data, delivery_info, metadata)
   rescue => e
-    error_messaging(e, delivery_info)
+    error_messaging(e.backtrace, delivery_info)
     ack!
   end
 end
---
+++ spec/links_publisher_worker_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe LinksPublisherWorker, type: :worker do
+  let!(:payload) { ['https://example_1.com?download%params',
+    'https://example_2.com?download%params',
+    'https://example_3.com?download%params'] }
+
+  let(:routing_key) { 'parsed' }
+  let(:metadata) { {:content_type=>"application/octet-stream", :delivery_mode=>2, :priority=>0}}
+  let(:worker_processed_good) { described_class.new.work_with_params('', { routing_key: routing_key }, metadata)}
+
+  let(:worker_processed_bad) { described_class.new.work_with_params('', {}, {})}
+
+
+  context 'successful deliver to consumers' do
+    it 'successfully sends payload to a consumer with ack' do
+      expect(worker_processed_good).to eq(:ack)
+      worker_processed_good
+    end
+  end
+
+  context 'errors in delivering process' do
+    it 'interrups delivering and logs full error trace' do
+      expect(Sneakers.logger).to receive(:error).with(a_string_including("stopped delivering at"))
+      worker_processed_bad
+    end
+  end
+end
