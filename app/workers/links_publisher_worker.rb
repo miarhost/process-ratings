84c6c1dd6493f2d9a96c44416f641f6e014b232c
rghost <miarhost@gmail.com>
2024-06-27 17:46:15 +0300

    Update parsed links publisher

--- app/workers/links_publisher_worker.rb
+++ app/workers/links_publisher_worker.rb
@@ -2,16 +2,17 @@ require 'sneakers'
 class LinksPublisherWorker
   include Sneakers::Worker
   include SneakersLogging
-
-  from_queue 'parsed.links', threads: 11, prefetch: 11, timeout_job_after: 1
+  from_queue 'parsed.links'
 
   def work_with_params(message, delivery_info, metadata)
-    encoded_msg = Sneakers::ContentType.serialize(message, 'application/json')
-    Sneakers::Publisher.new(exchange: 'snickers', exchange_type: 'direct', heartbeat: 1)
-                       .publish(encoded_msg, routing_key: 'parsed.links', content_type: 'application/json')
+    self.class.enqueue(message, routing_key: , content_type: 'application/json', content_encoding: 'deflate', to_queue: routing_key)
     logging(message, delivery_info, metadata)
   rescue => e
     error_messaging(e.backtrace, delivery_info)
     ack!
+  end
+
+  def routing_key
+    'parsed.links'
   end
 end
